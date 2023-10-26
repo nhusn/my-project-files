58b892fe43825505ebdc1057061531ba0486ed4f
nhusn <139863367+nhusn@users.noreply.github.com>
2023-10-26 13:23:45 +0530

    designing

---
+++ src/Components/ProjectCard.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Card, Modal, Col, Row, Button } from 'react-bootstrap';
+import projectPic from '../Assets/mediaPlayer.png';
+import { Link } from 'react-router-dom';
+
+
+function ProjectCard() {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+  return (
+    <>
+      
+          <Card className='shadow mb-3' onClick={handleShow}>
+            <Card.Img variant="top" src={projectPic} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+            </Card.Body>
+          </Card>
+
+          <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Row>
+                <Col md={6}>
+                    <img className='w-100' src={projectPic} alt="" />
+                </Col>
+                <Col md={6}>
+                    <h2>Project Title</h2>
+                    <p>Project Overview: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis nulla provident voluptates nihil fugit ipsa delectus. Earum repudiandae repellat perferendis soluta repellendus perspiciatis labore itaque, dolor, blanditiis officiis natus porro?</p>
+                    <p>Language used:<span className='fw-bolder'>HTML,CSS,React</span></p>
+                </Col>
+            </Row>
+            <div className='d-flex' style={{gap:"10px"}}>
+                <Link to={'https://github.com/nhusn/Veedio'}><i class="fa-brands fa-github fa-xl"></i></Link>
+                <Link to={'https://cerulean-belekoy-bc4473.netlify.app/'}><i class="fa-solid fa-globe fa-xl"></i></Link>
+            </div>
+        </Modal.Body>
+      </Modal>
+      
+    </>
+  )
+}
+
+export default ProjectCard
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Col, Row } from 'reactstrap'
+import titleImg from '../Assets/title.png'
+import ProjectCard from '../Components/ProjectCard'
+import { Link } from 'react-router-dom'
+
+function Home() {
+  return (
+    <>
+      <div style={{width:"100%",height:"100vh"}} className='bg-secondary container-fluid rounded'>
+        <Row className='align-items-center p-5'>
+          <Col sm={12} md={6}>
+            <h1 style={{fontSize:"80px"}} className='fw-bolder text-light'><i class="fa-brands fa-algolia fa-fade"></i> Project Fair</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur blanditiis minus unde officia. Distinctio rerum non ab. Possimus, molestiae! Voluptatum est expedita tenetur accusantium quia explicabo, soluta laboriosam deserunt cupiditate.
+            Voluptatem rem debitis dolor, non enim magnam, laudantium est placeat numquam dolore magni iste laborum. Temporibus quia incidunt omnis nihil deserunt voluptate ratione sunt, autem necessitatibus neque nam fugit placeat!</p>
+            <button className='btn btn-warning'>Start to explore</button>
+          </Col>
+
+          <Col sm={12} md={6} className='text-center'>
+            <img className='w-75' src={titleImg} alt="" />
+          </Col>
+        </Row>
+      </div>
+
+      <div className='all-projects mt-5'>
+        <h1 className='text-center mb-5'>Explore Our Projects</h1>
+        <marquee scrollAmount={25}>
+          <Row>
+            <Col sm={12} md={6} lg={4}>
+              <ProjectCard/>
+            </Col>
+            <Col sm={12} md={6} lg={4}>
+              <ProjectCard/>
+            </Col>
+            <Col sm={12} md={6} lg={4}>
+              <ProjectCard/>
+            </Col>
+          </Row>
+        </marquee>
+        <div className='text-center mt-5 mb-3'><Link to={'/projects'}>view more projects</Link></div>
+      </div>
+    </>
+  )
+}
+
+export default Home
